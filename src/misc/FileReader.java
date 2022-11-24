cbb87184277c9b581731eaab23071c41df920c35
Marcel Streicher <marcel.streicher58@gmail.com>
2022-11-24 13:41:22 +0100

    finished 2017.01 added FileReader.getInputAsIntegerList

--- src/misc/FileReader.java
+++ src/misc/FileReader.java
@@ -3,10 +3,19 @@ package misc;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileReader {
     public static List<String> readInput(String filename) throws IOException {
         return Files.readAllLines(Paths.get(filename));
+    }
+
+    public static List<Integer> getInputAsIntegerList(String filename) throws IOException {
+        List<Integer> output = new ArrayList<>();
+        for (int i : readInput(filename).get(0).toCharArray()) {
+            output.add(i - 48);
+        }
+        return output;
     }
 }
