891a3a873dd42c6a083d0ce866dbe57137cd3b94
Marcel Streicher <marcel.streicher58@gmail.com>
2022-11-24 23:11:31 +0100

    finished 2019.02

--- src/aoc2019/Day02.java
+++ src/aoc2019/Day02.java
@@ -3,6 +3,7 @@ package aoc2019;
 import misc.FileReader;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,6 +11,7 @@ import java.util.stream.Collectors;
 public class Day02 {
     public static void main(String[] args) throws IOException {
         problem1("resources/2019/2019_02.txt");
+        problem2("resources/2019/2019_02.txt");
     }
 
     public static void problem1(String filename) throws IOException {
@@ -23,6 +25,23 @@ public class Day02 {
         Intcode intcode = new Intcode(input);
         while (intcode.getTask() != Intcode.Task.HALT) {
             intcode.executeTask();
+        }
+    }
+
+    public static void problem2(String filename) throws IOException {
+        List<Integer> input = Arrays.stream(FileReader.readInput(filename).get(0).split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        for (int i = 0; i <= 99; i++) {
+            for (int j = 0; j <= 99; j++) {
+                input.set(1, i);
+                input.set(2, j);
+                Intcode intcode = new Intcode(new ArrayList<>(input), 19690720);
+                while (intcode.getTask() != Intcode.Task.HALT) {
+                    intcode.executeTask();
+                }
+            }
         }
     }
 
@@ -51,9 +70,15 @@ public class Day02 {
         private int instructionPointer;
         private List<Integer> codes;
         private Task task;
+        private int required;
 
         public Intcode(List<Integer> codes) {
+            this(codes, -1);
+        }
+
+        public Intcode(List<Integer> codes, int required) {
             this.codes = codes;
+            this.required = required;
         }
 
         public void executeTask() {
@@ -74,7 +99,6 @@ public class Day02 {
                 case 99:
                     this.task = Task.HALT;
                     endProgram();
-                    System.exit(0);
                     break;
             }
             codes.set(target, this.task.action(a, b));
@@ -85,7 +109,12 @@ public class Day02 {
         }
 
         private void endProgram() {
-            System.out.printf("Programm ended. Value at pos 0: %d\n", codes.get(0));
+            if (codes.get(0) == this.required) {
+                System.out.printf("Programm ended. Value at pos 0: %d\nnoun=%d, verb=%d -> %d",
+                        codes.get(0), codes.get(1), codes.get(2), 100 * codes.get(1) + codes.get(2));
+            } else if (this.required == -1) {
+                System.out.printf("Programm ended. Value at pos 0: %d\n", codes.get(0));
+            }
         }
     }
 }
