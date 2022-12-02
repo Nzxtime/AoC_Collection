003fa629c324a6a918467445f2642163f0ca83d5
Marcel Streicher <marcel.streicher58@gmail.com>
2022-12-02 02:09:13 +0100

    finished 2019.05 part 1 (part 2 bugs somehow) (took me way too long) old (and better) solution: https://github.com/Nzxtime/AdventOfCode2019-Python/blob/master/src/Day5/Part1.py

--- src/aoc2019/Day05.java
+++ src/aoc2019/Day05.java
@@ -1,44 +1,38 @@
 package aoc2019;
 
+import com.sun.security.jgss.GSSUtil;
 import misc.FileReader;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Day05 {
     public static void main(String[] args) throws IOException {
-        problem1("resources/2019/2019_05.txt");
+        //problem1("resources/2019/2019_05.txt");
         problem2("resources/2019/2019_05.txt");
     }
 
     public static void problem1(String filename) throws IOException {
-        List<Integer> input = Arrays.stream(FileReader.readInput(filename).get(0).split(","))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        List<Integer> input = Arrays.stream(FileReader.readInput(filename).get(0).split(",")).map(Integer::parseInt).collect(Collectors.toList());
 
         Intcode intcode = new Intcode(input);
+
         while (intcode.getTask() != Intcode.Task.HALT) {
             intcode.executeTask(1);
         }
     }
 
     public static void problem2(String filename) throws IOException {
-        List<Integer> input = Arrays.stream(FileReader.readInput(filename).get(0).split(","))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        List<Integer> input = Arrays.stream(FileReader.readInput(filename).get(0).split(",")).map(Integer::parseInt).collect(Collectors.toList());
 
-        for (int i = 0; i <= 99; i++) {
-            for (int j = 0; j <= 99; j++) {
-                input.set(1, i);
-                input.set(2, j);
-                Intcode intcode = new Intcode(new ArrayList<>(input), 19690720);
-                while (intcode.getTask() != Intcode.Task.HALT) {
-                    intcode.executeTask(1);
-                }
-            }
+        Intcode intcode = new Intcode(input);
+
+        while (intcode.getTask() != Intcode.Task.HALT) {
+            intcode.executeTask(5);
         }
     }
 
@@ -54,6 +48,17 @@ public class Day05 {
                 int action(int a, int b) {
                     return a * b;
                 }
+            }, INPUT {
+                @Override
+                int action(int a, int b) {
+                    return a;
+                }
+            }, OUTPUT {
+                @Override
+                int action(int a, int b) {
+                    System.out.println(a);
+                    return 0;
+                }
             }, HALT {
                 @Override
                 int action(int a, int b) {
@@ -64,7 +69,7 @@ public class Day05 {
             abstract int action(int a, int b);
         }
 
-        private int instructionPointer;
+        private int instructionPointer = 0;
         private List<Integer> codes;
         private Task task;
         private int required;
@@ -80,17 +85,58 @@ public class Day05 {
 
         public void executeTask(int input) {
             int task = codes.get(this.instructionPointer);
-            int a = codes.get(codes.get(this.instructionPointer + 1));
-            int b = codes.get(codes.get(this.instructionPointer + 2));
-            int target = codes.get(this.instructionPointer + 3);
+            int opcode = task % 100;
 
-            switch (task) {
+            int[] parameterModes = getParameterModes(task);
+
+            int a = parameterModes[0] == 0 ? codes.get(this.instructionPointer + 1) : this.instructionPointer + 1;
+            int b = parameterModes[1] == 0 ? codes.get(this.instructionPointer + 2) : this.instructionPointer + 2;
+            int c = parameterModes[2] == 0 ? codes.get(this.instructionPointer + 3) : this.instructionPointer + 3;
+            //System.out.printf("task: %d, opcode: %d, parameterModes: %s\n   %d, %d, %d\n", task, opcode, Arrays.toString(parameterModes), a, b, c);
+            System.out.printf("task: %d, opcode: %d, modes: %s, a: %d,b: %d,c: %d\n", task, opcode, Arrays.toString(parameterModes), a, b, c);
+
+            switch (opcode) {
                 case 1:
                     this.task = Task.ADD;
+                    codes.set(codes.get(this.instructionPointer + 3), this.task.action(codes.get(a), codes.get(b)));
                     this.instructionPointer += 4;
                     break;
                 case 2:
                     this.task = Task.MULTIPLY;
+                    codes.set(codes.get(this.instructionPointer + 3), this.task.action(codes.get(a), codes.get(b)));
+                    this.instructionPointer += 4;
+                    break;
+                case 3:
+                    this.task = Task.INPUT;
+                    codes.set(codes.get(this.instructionPointer + 1), input);
+                    this.instructionPointer += 2;
+                    break;
+                case 4:
+                    this.task = Task.OUTPUT;
+                    this.task.action(codes.get(this.instructionPointer + 1), b);
+                    this.instructionPointer += 2;
+                    break;
+                case 5:
+                    if (codes.get(a) != 0) this.instructionPointer = codes.get(b);
+                    else this.instructionPointer += 3;
+                    break;
+                case 6:
+                    if (codes.get(a) == 0) {
+                        System.out.println("-" + codes.get(a) + "-(0)" + this.instructionPointer);
+                        this.instructionPointer = codes.get(b);
+                    } else {
+                        System.out.println("-" + codes.get(a) + "-(!0)" + this.instructionPointer);
+                        this.instructionPointer += 3;
+                    }
+                    break;
+                case 7:
+                    codes.set(codes.get(c), codes.get(a) < codes.get(b) ? 1 : 0);
+                    this.instructionPointer += 4;
+                    break;
+                case 8:
+                    int value1 = codes.get(a);
+                    int value2 = codes.get(b);
+                    codes.set(codes.get(c), value1 == value2 ? 1 : 0);
                     this.instructionPointer += 4;
                     break;
                 case 99:
@@ -98,7 +144,28 @@ public class Day05 {
                     endProgram();
                     break;
             }
-            codes.set(target, this.task.action(a, b));
+        }
+
+        public int[] getParameterModes(int task) {
+            List<Integer> output = new ArrayList<>();
+
+            List<Integer> digits = getDigits(task);
+
+            for (int i = 2; i < digits.size(); i++) {
+                output.add(digits.get(i));
+            }
+            while (output.size() < 3) output.add(0);
+
+            return output.stream().mapToInt(Integer::intValue).toArray();
+        }
+
+        public static List<Integer> getDigits(int a) {
+            List<Integer> output = new ArrayList<>();
+            while (a > 0) {
+                output.add(a % 10);
+                a /= 10;
+            }
+            return output;
         }
 
         public Task getTask() {
@@ -107,8 +174,7 @@ public class Day05 {
 
         private void endProgram() {
             if (codes.get(0) == this.required) {
-                System.out.printf("Programm ended. Value at pos 0: %d\nnoun=%d, verb=%d -> %d",
-                        codes.get(0), codes.get(1), codes.get(2), 100 * codes.get(1) + codes.get(2));
+                System.out.printf("Programm ended. Value at pos 0: %d\nnoun=%d, verb=%d -> %d", codes.get(0), codes.get(1), codes.get(2), 100 * codes.get(1) + codes.get(2));
             } else if (this.required == -1) {
                 System.out.printf("Programm ended. Value at pos 0: %d\n", codes.get(0));
             }
