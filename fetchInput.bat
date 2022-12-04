60c356e13a72ce8233a3097c3dd63cd6c4d9af2b
Marcel Streicher <marcel.streicher58@gmail.com>
2022-12-04 20:06:14 +0100

    created bat-file which fetches input for a given day

---
+++ fetchInput.bat
@@ -0,0 +1,18 @@
+@echo off
+Setlocal EnableDelayedExpansion
+
+if [%1]==[] if [%2]==[] (
+    echo "specify year and day"
+    exit
+)
+
+set year=%1
+set day=%2
+set day=00%day%
+set day=%day:~-2%
+
+echo %year%
+echo %day%
+
+.\aocdl.exe -year %year% -day %day% -output "resources/%year%/%year%_%day%.txt"
+git add "resources/%year%/%year%_%day%.txt"
