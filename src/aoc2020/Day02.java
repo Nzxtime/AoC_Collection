a00274e5c369c6e0efd9e195a86d7008713a772f
Marcel Streicher <marcel.streicher58@gmail.com>
2022-12-08 22:14:22 +0100

    finished 2020.02

---
+++ src/aoc2020/Day02.java
@@ -0,0 +1,73 @@
+package aoc2020;
+
+import misc.FileReader;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day02 {
+    public static void main(String[] args) throws IOException {
+        System.out.println(problem1("resources/2020/2020_02.txt"));
+        System.out.println(problem2("resources/2020/2020_02.txt"));
+    }
+
+    private static int problem1(String filename) throws IOException {
+        List<String> input = FileReader.readInput(filename);
+        Pattern pattern = Pattern.compile("(?<min>\\d+)-(?<max>\\d+) (?<character>.): (?<password>.+)");
+        Matcher matcher;
+        int counter = 0;
+
+        for (String s : input) {
+            matcher = pattern.matcher(s);
+
+            if (matcher.matches()) {
+                int min = Integer.parseInt(matcher.group("min"));
+                int max = Integer.parseInt(matcher.group("max"));
+                char c = matcher.group("character").charAt(0);
+                String password = matcher.group("password");
+
+                Map<Character, Integer> characterIntegerMap = generateCharacterMap(password);
+                if (!characterIntegerMap.containsKey(c)) continue;
+                if (min <= characterIntegerMap.get(c) && max >= characterIntegerMap.get(c)) counter++;
+            }
+        }
+        return counter;
+    }
+
+    private static int problem2(String filename) throws IOException {
+        List<String> input = FileReader.readInput(filename);
+        Pattern pattern = Pattern.compile("(?<pos1>\\d+)-(?<pos2>\\d+) (?<character>.): (?<password>.+)");
+        Matcher matcher;
+        int counter = 0;
+
+        for (String s : input) {
+            matcher = pattern.matcher(s);
+
+            if (matcher.matches()) {
+                int pos1 = Integer.parseInt(matcher.group("pos1")) - 1;
+                int pos2 = Integer.parseInt(matcher.group("pos2")) - 1;
+                char c = matcher.group("character").charAt(0);
+                String password = matcher.group("password");
+
+                if (pos1 >= password.length() || pos2 >= password.length()) continue;
+                if ((password.charAt(pos1) == c && password.charAt(pos2) != c) || (password.charAt(pos1) != c && password.charAt(pos2) == c))
+                    counter++;
+            }
+        }
+        return counter;
+    }
+
+    private static Map<Character, Integer> generateCharacterMap(String s) {
+        Map<Character, Integer> output = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            output.merge(c, 1, Integer::sum);
+        }
+
+        return output;
+    }
+}
