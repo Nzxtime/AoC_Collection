c4a601e7447ec1347fa0cfface551bf74a6032c0
Marcel Streicher <marcel.streicher58@gmail.com>
2023-12-06 16:56:45 +0100

    finished 2023.06 part 2 with brute-force

--- src/aoc2023/Day06.java
+++ src/aoc2023/Day06.java
@@ -3,17 +3,15 @@ package aoc2023;
 import misc.FileReader;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Day06 {
     public static void main(String[] args) throws IOException {
         System.out.println(problem1("resources/2023/2023_06.txt"));
+        System.out.println(problem2("resources/2023/2023_06.txt"));
     }
 
-    private static int[][] getInputs(List<String> lines) {
+    private static int[][] getSplitInputs(List<String> lines) {
         int size = lines.get(0).split(":")[1].split("\\s+").length;
         int[][] output = new int[size - 1][2];
 
@@ -28,7 +26,7 @@ public class Day06 {
     }
 
     private static int problem1(String filename) throws IOException {
-        int[][] data = getInputs(FileReader.readInput(filename));
+        int[][] data = getSplitInputs(FileReader.readInput(filename));
         int[] scores = new int[data.length];
 
         for (int i = 0; i < data.length; i++) {
@@ -49,5 +47,21 @@ public class Day06 {
         }
 
         return product;
+    }
+
+    private static long problem2(String filename) throws IOException {
+        List<String> lines = FileReader.readInput(filename);
+        int score = 0;
+        long time = Long.parseLong(lines.get(0).split(":")[1].replaceAll("\\s+", ""));
+        long distance = Long.parseLong(lines.get(1).split(":")[1].replaceAll("\\s+", ""));
+
+        for (int j = 1; j < time; j++) {
+            long remainingTime = time - j;
+            long cDistance = remainingTime * j;
+            if (cDistance < distance) continue;
+            score++;
+        }
+
+        return score;
     }
 }
