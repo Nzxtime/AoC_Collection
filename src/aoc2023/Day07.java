4ffcd31bff1b79221cc59cc3df34ed70d374038f
Marcel Streicher <marcel.streicher58@gmail.com>
2023-12-07 22:22:01 +0100

    finished 2023.07 - not proud of this garbage

--- src/aoc2023/Day07.java
+++ src/aoc2023/Day07.java
@@ -8,16 +8,25 @@ import java.util.*;
 
 public class Day07 {
     public static void main(String[] args) throws IOException {
-        //System.out.println(problem1("resources/2023/test.txt"));
         System.out.println(problem1("resources/2023/2023_07.txt"));
+        System.out.println(problem2("resources/2023/2023_07.txt"));
     }
 
     private static long problem1(String filename) throws IOException {
         List<Hand> hands = Hand.getHands(FileReader.readInput(filename));
-
-        //System.out.printf("before: %s\n", hands);
         hands.sort(Hand::compareTo);
-        //System.out.printf("sorted: %s\n", hands);
+
+        long sum = 0;
+        for (int i = 0; i < hands.size(); i++) {
+            sum += (long) (i + 1) * hands.get(i).bid;
+        }
+
+        return sum;
+    }
+
+    private static long problem2(String filename) throws IOException {
+        List<Hand2> hands = Hand2.getHands2(FileReader.readInput(filename));
+        hands.sort(Hand::compareTo);
 
         long sum = 0;
         for (int i = 0; i < hands.size(); i++) {
@@ -30,6 +39,7 @@ public class Day07 {
     private static class Hand implements Comparable<Hand> {
         String hand;
         int bid;
+        int strength;
 
         static Map<Character, Integer> cards = new HashMap<>();
 
@@ -52,6 +62,7 @@ public class Day07 {
         public Hand(String line) {
             this.hand = line.split("\\s")[0];
             this.bid = Integer.parseInt(line.split("\\s")[1]);
+            this.strength = getStrength();
         }
 
         static List<Hand> getHands(List<String> input) {
@@ -61,9 +72,9 @@ public class Day07 {
         }
 
         int getStrength() {
-            Map<Character, Integer> set = Helper.generateCharacterMap(this.hand);
-            List<Integer> amounts = set.values().stream().sorted(Comparator.reverseOrder()).toList();
-            switch (set.size()) {
+            Map<Character, Integer> map = Helper.generateCharacterMap(this.hand);
+            List<Integer> amounts = map.values().stream().sorted(Comparator.reverseOrder()).toList();
+            switch (map.size()) {
                 case 5 -> {
                     return 1;
                 }
@@ -95,13 +106,13 @@ public class Day07 {
 
         @Override
         public String toString() {
-            return String.format("%s - %d", this.hand, this.bid);
+            return String.format("%s - %d - %d", this.hand, this.bid, this.strength);
         }
 
         @Override
         public int compareTo(Hand o) {
-            int a = this.getStrength();
-            int b = o.getStrength();
+            int a = this.strength;
+            int b = o.strength;
             if (a > b) return 1;
             else if (a < b) return -1;
             for (int i = 0; i < this.hand.length(); i++) {
@@ -110,6 +121,63 @@ public class Day07 {
                 if (c != d) return compareCards(c, d);
             }
             return 0;
+        }
+    }
+
+    static class Hand2 extends Hand {
+        int strength = 0;
+
+        static {
+            Hand.cards.put('J', 1);
+        }
+
+        public Hand2(String line) {
+            super(line);
+            this.strength = getStrength();
+        }
+
+        static List<Hand2> getHands2(List<String> input) {
+            List<Hand2> output = new ArrayList<>();
+            input.forEach(x -> output.add(new Hand2(x)));
+            return output;
+        }
+
+        @Override
+        int getStrength() {
+            Map<Character, Integer> map = Helper.generateCharacterMap(this.hand);
+            List<Integer> amounts = new ArrayList<>(map.values().stream().sorted(Comparator.reverseOrder()).toList());
+            int joker = map.getOrDefault('J', 0);
+            if (joker == 0) return super.getStrength();
+            List<Character> keys = new ArrayList<>(map.keySet());
+            keys.removeIf(x -> x.equals('J'));
+            amounts.set(0, amounts.get(0) + joker);
+
+            switch (keys.size()) {
+                case 5 -> {
+                    return 1;
+                }
+                case 4 -> {
+                    return 2;
+                }
+                case 3 -> {
+                    if (amounts.get(0).equals(amounts.get(1))) {
+                        return 3;
+                    }
+                    return 4;
+                }
+                case 2 -> {
+                    if (amounts.get(0) == 3) return 5;
+                    return 6;
+                }
+
+                case 1, 0 -> {
+                    return 7;
+                }
+
+                default -> {
+                    return 0;
+                }
+            }
         }
     }
 }
