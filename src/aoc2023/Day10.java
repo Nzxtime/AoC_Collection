9feb26b52559fdd58564540bae3ec92bf155d844
Marcel Streicher <marcel.streicher58@gmail.com>
2023-12-10 18:38:42 +0100

    started 2023.10 a little bit

---
+++ src/aoc2023/Day10.java
@@ -0,0 +1,62 @@
+package aoc2023;
+
+import misc.FileReader;
+import misc.Point;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day10 {
+    public static void main(String[] args) throws IOException {
+        System.out.println(problem1("resources/2023/test.txt"));
+        //System.out.println(problem1("resources/2023/2023_10.txt"));
+    }
+
+    private static int problem1(String filename) throws IOException {
+        Maze maze = new Maze(FileReader.getInputAsCharArray(filename));
+
+        System.out.println(maze);
+
+        return 0;
+    }
+
+    private static class Maze {
+        private char[][] maze;
+
+        private char[][] loop;
+
+        private Point startPos;
+
+        private static Map<Character, Point> move = new HashMap<>();
+
+        static {
+            move.put('|', new Point(1, 0));
+            move.put('-', new Point(0, 1));
+            move.put('L', new Point(1, 1));
+            move.put('J', new Point(1, -1));
+            move.put('7', new Point(-1, -1));
+            move.put('F', new Point(-1, 1));
+        }
+
+        public Maze(char[][] maze) {
+            this.maze = maze;
+            this.loop = new char[maze.length][maze[0].length];
+
+            for (int i = 0; i < maze.length; i++) {
+                for (int j = 0; j < maze[i].length; j++) {
+                    if (maze[i][j] != 'S') continue;
+                    this.startPos = new Point(i, j);
+                    this.loop[i][j] = maze[i][j];
+                    return;
+                }
+            }
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Maze={%s}", Arrays.deepToString(maze));
+        }
+    }
+}
